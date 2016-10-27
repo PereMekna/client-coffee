ca26d816a028aedbe71051e48165ae360c66d762
PereMekna <dupont.louis4@gmail.com>
2016-10-27 23:00:07 +0200

    Made authentification working with token

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,15 +10,42 @@
 angular.module('testApp')
   .controller('LoginCtrl', function ($scope, $http) {
 
+  	var token = 0;
+
   	$scope.logged = false;
+  	$scope.loading = false;
+
+  	var getUserProfile = function(userId) {
+  		$http.get('http://reggaeshark.eu:3000/api/Users/' + userId + '?access_token=' + token)
+  		.then(function(response){
+  			$scope.username = response.data.username;
+  		}, function(error){
+  			console.log(error);
+  		});
+  	}
+
+  	$scope.logout = function() {
+  		$scope.loading = true;
+  		$http.post('http://reggaeshark.eu:3000/api/Users/logout?access_token=' + token).then(function(response){
+  			$scope.loading = false;
+  			$scope.logged = false;
+  		}, function(error) {
+  			$scope.loading = false;
+  			console.log('error', error);
+  		});
+  	}
 
   	$scope.connectUser = function() {
+  		$scope.loading = true;
   	  $http.post('http://reggaeshark.eu:3000/api/Users/login', {'email' : $scope.email, 'password': $scope.password})
   	  .then(function(response){
+  	  	$scope.loading = false;
   	  	$scope.logged = true;
-  	    console.log("success", response);
+  	  	token = response.data.id;
+   	  	getUserProfile(response.data.userId);
   	  }, function(error){
-  	    console.log("error", error);
+  	  	$scope.loading = false;
+  	  	console.log(error);
   	  });
   	};
   });
